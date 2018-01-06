09393731973b28a0916775c19dd49f1024d2d61e
sandeep mahto <sandeep.mahto@opstree.com>
2018-01-06 14:49:01 +0530

    final testing done

--- services/src/jobs/Build_Images_And_Push_To_ECR.groovy
+++ services/src/jobs/Build_Images_And_Push_To_ECR.groovy
@@ -1,8 +1,8 @@
 pipelineJob('Build-Images-And-Push-To-ECR') {
   parameters {
-    stringParam('REPOSITORY_CLONE_ADDRESS', 'git@github.com:nclouds/test.git', 'ssh clone address of the repository where dockerfile is present')
+    stringParam('REPOSITORY_CLONE_ADDRESS', 'git@github.com:opstree-aws-utils/ecs-cfn.git', 'ssh clone address of the repository where dockerfile is present')
     stringParam('BRANCH_NAME', 'master', 'branch name to build the docker image')
-    stringParam('REGISTRY_NAME', 'https://123456012.dkr.ecr.us-east-1.amazonaws.com', 'name of the ecr')
+    stringParam('REGISTRY_NAME', 'https://405349833286.dkr.ecr.us-east-1.amazonaws.com/jenkins', 'name of the ecr')
   }
   environmentVariables {
       env('slaveName', slaveName)
